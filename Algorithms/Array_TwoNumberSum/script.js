1f17418e443d6bb960cad3b660803b35f4f9a30e
35115634 <bischof.chris@gmail.com>
2019-11-23 17:34:37 +0100

    added second variation of solving TwoNumberSum js scirpt

--- Algorithms/Array_TwoNumberSum/script.js
+++ Algorithms/Array_TwoNumberSum/script.js
@@ -13,6 +13,28 @@ function Array_TwoNumberSum(array, expSum) {
   return [];
 }
 
+//saving occured numbers
+function Array_TwoNumberSum_Save(array, expSum) {
+  let occured = {};
+  let missingNum;
+  for (i of array) {
+    missingNum = expSum - i;
+    if (occured.hasOwnProperty(missingNum)) {
+      return [missingNum, i];
+    } else {
+      occured[i] = true;
+    }
+  }
+  return [];
+}
+
+//sorting and testing out if value needs to become lower or higher
+//js array.prototype.sort() works in-place
+
 // test with the sample input from description
 console.log(Array_TwoNumberSum([4, 1, -5, 7, 12, 23, -3, 13], 10));
 console.log(Array_TwoNumberSum([4, 1, -5, 7, 12, 23, -3, 14], 10));
+
+//test second variation: with saving the occured numbers in an js object
+console.log(Array_TwoNumberSum_Save([4, 1, -5, 7, 12, 23, -3, 13], 10));
+console.log(Array_TwoNumberSum_Save([4, 1, -5, 7, 12, 23, -3, 14], 10));
